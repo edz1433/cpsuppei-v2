8287e0f89b646ce394739ec5060a63d574a7732b
edz1433 <edzavril1@gmail.com>
2025-06-30 12:16:18 +0800

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,20 +13,23 @@ class LoginController extends Controller
 
     public function postLogin(Request $request){
         $request->validate([
-            'username'=>'required',
-            'password'=>'required|min:5'
+            'username' => 'required',
+            'password' => 'required|min:5'
         ]);
 
-        $validated=auth()->attempt([
-            'username'=>$request->username,
-            'password'=>$request->password,
-        ],$request->password);
-
-        if($validated){
-            return redirect()->route('dashboard')->with('success','Login Successfully');
-        }else{
-            return redirect()->back()->with('error','Invalid Credentials');
-            //console.log($validated);
+        // Attempt to authenticate the user
+        if (auth()->attempt([
+            'username' => $request->username,
+            'password' => $request->password,
+        ])) {
+            $user = auth()->user();
+            if ($user->role === 'Campus Admin') {
+                auth()->logout();
+                return redirect()->back()->with('error', 'Your account is disabled at this time.');
+            }
+            return redirect()->route('dashboard')->with('success', 'Login Successfully');
+        } else {
+            return redirect()->back()->with('error', 'Invalid Credentials');
         }
     }
 }
