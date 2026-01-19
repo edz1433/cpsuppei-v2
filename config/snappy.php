b88fa422249473e32f217874b6a16f48a0361f26
edz1433 <edzavril1@gmail.com>
2026-01-19 16:05:30 +0800

    update

--- config/snappy.php
+++ config/snappy.php
@@ -4,10 +4,7 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        'binary' => env(
-            'WKHTMLTOPDF_PRODUCTION',               // Linux / production path from .env
-            '"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"' // fallback for local Windows
-        ),
+        'binary'  => '"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"',
         'timeout' => false,
         'options' => [
             'enable-local-file-access' => true,
